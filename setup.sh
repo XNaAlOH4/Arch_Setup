c6812186bcbb5fdf360a7e33bc8ede2a9e449ecd
XNaAlOH4 <xnaaloh4x@gmail.com>
2025-11-25 16:58:39 +0800

    Removed some lines from setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,6 @@ $START NetworkManager.service
 
 #Blocking ads
 #List of known ads sites
-#sin3-ib.adnxs.com
-#googleads.g.doubleclick.net
-#insight.adsrvr.org
-#googleadservices.com
 
 #ads_ip=('103.43.0.0/16'
 #	'74.125.0.0/16' '172.217.194.0/24, 142.251.0.0/16' ## These ones are for the google ads but they also overlap with the google servers so feel free to remove them if google ads don't get in the way
